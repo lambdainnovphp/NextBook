01a3df8b1cd5b2f07def7dfca85046bc49369ba3
lambdainnovphp <mydihemasundar@lambdainnov.com>
2015-11-30 18:26:07 +0530

    expenses section

--- Expenses.php
+++ Expenses.php
@@ -6,9 +6,9 @@ $res_Usql=mysql_fetch_assoc(mysql_query($Usql));
 
 if($_POST['submit']=="Add Expense")
 {
-    print_r($_POST); exit;
-	$Exsql="INSERT INTO Expenses(ExpenseName,Category,vendor,Amount,Expen_Date,Notes) VALUES('".$_POST['Extitle']."','".$_POST['Excategory']."','".$_POST['ExVendor']."','".$_POST['Amount']."','".$_POST['ExDate']."','".$_POST['Exnotes']."')"; 
+	$Exsql="INSERT INTO Expenses(UserId,Extitle,category,vendor,Amount,ExpenseDate,Notes,status) VALUES('".$_SESSION['Userid']."','".$_POST['Extitle']."','".$_POST['Excategory']."','".$_POST['ExVendor']."','".$_POST['Amount']."','".$_POST['ExDate']."','".$_POST['Exnotes']."','active')"; 
 	$Exquery = mysql_query($Exsql);
+    header('location:Expenses.php');
 }
 ?>
 <style>
@@ -25,8 +25,8 @@ small{
 </style>
 	<div id="container">
 		Dashboard<br>
-        	Welcome <?php echo $res_Usql['Firstname'];?>    
-            <span id="newC" style="float:right; background-color:#ccc; padding:5px; cursor:pointer;">Create New Expense</span>
+    	Welcome <?php echo $res_Usql['Firstname'];?>    
+        <span id="newC" style="float:right; background-color:#ccc; padding:5px; cursor:pointer;">Create New Expense</span>
         <div id="CreateCl" style="display:none; padding-top:10px;">
         	<strong>New Expense</strong><br>
             <form name="NewExpense" method="post">
@@ -35,7 +35,7 @@ small{
                         <td>Title</td><td>: <input type="text" name="Extitle" id="Extitle" required></td>
                     </tr>
                     <tr>
-                        <td>Amount</td><td>: <input type="text" name="Amount" id="Amount" required></td>
+                        <td>Amount</td><td>: <input type="text" name="Amount" id="Amount" required onkeypress="return IsNumeric(event)"></td>
                     </tr>
                     <tr>
                         <td>Date</td><td>: <input type="text" name="ExDate" id="ExDate" required></td>
@@ -58,42 +58,127 @@ small{
                 </table>
             </form>
             
-        </div>    
-        
-        <div id="showclient">
-        	<table cellspacing="20">
-            <thead>
-                <tr class="filters">
-                    <th>S.No</th>
-                    <th>Date</th>
-                    <th>Category</th>
-                    <th>Notes</th>
-                    <th>Amount</th>
-                </tr>               
-            </thead>
-            <tbody>
-            <?php 
-            $ViewExsql = "SELECT * FROM Expenses";
-            $Exresult = mysql_query($ViewExsql);
-            $Ekl = 1;
-            while($Exrow = mysql_fetch_array($Exresult)){ ?>               
-                <tr>
-                	<td><?php echo $jkl;?></td>
-                    <td><?php echo $Exrow['Expen_Date']?></td>
-                    <td><?php echo $Exrow['Category']?></td>
-                    <td><?php echo $Exrow['Notes']?></td>
-                    <td><?php echo $Exrow['Amount']?></td>
-                </tr>
-            <?php $Ekl++; }?>											
-                
-            </tbody>
-        </table>
-        	
         </div>
+
+        <?php if($_GET['click']!="Edit" && $_GET['SortExp']==""){?>
+        
+            <div id="showclient" style="margin-top:10px;">
+            	<table cellspacing="20">
+                    <thead>
+                        <tr class="filters">
+                            <th>S.No</th>
+                            <th>Date</th>
+                            <th>Category</th>
+                            <th>Notes</th>
+                            <th>Amount</th>
+                            <th></th>
+                        </tr>               
+                    </thead>
+                    <tbody>
+                    <?php 
+                    $ViewExsql = "SELECT * FROM Expenses";
+                    $Exresult = mysql_query($ViewExsql);
+                    $Ekl = 1;
+                    while($Exrow = mysql_fetch_array($Exresult)){ ?>               
+                        <tr>
+                        	<td><?php echo $Ekl;?></td>
+                            <td><?php echo $Exrow['ExpenseDate']?></td>
+                            <td><?php echo $Exrow['category']?></td>
+                            <td><?php echo $Exrow['Notes']?></td>
+                            <td><?php echo $Exrow['Amount']?></td>
+                            <td><a href="Expenses.php?click=Edit&SortExp=<?php echo $Exrow['id']; ?>">Edit</a></td>
+                        </tr>
+                    <?php $Ekl++; }?>		
+                        
+                    </tbody>
+                </table>            	
+            </div>
+
+        <?php } else { 
+            $IndExp = mysql_query("Select * from expenses where id='".$_GET['SortExp']."'");
+            $Res_IndExp = mysql_fetch_assoc($IndExp);?>
+
+            <div id="editExpense" style="margin-top:10px;">
+                <form name="editExpform" method="post">
+                    <table>
+                        <tr>
+                            <td>Amount</td>
+                            <td>Date</td>
+                            <td>Vendor</td>
+                            <td>Category</td>
+                            <td>Notes</td>
+                        </tr>
+
+                        <tr>
+                            <td><input type="text" name="ExPAmount" id="ExPAmount" value="<?php echo $Res_IndExp['Amount'];?>" style="width:65px;" ></td>
+                            <td><input type="text" name="ExPDate" id="ExPDate" value="<?php echo $Res_IndExp['ExpenseDate'];?>" style="width:70px;" ></td>
+                            <td><input type="text" name="ExPVendor" id="ExPVendor" value="<?php echo $Res_IndExp['vendor'];?>" style="width:80px;" ></td>
+                            <td><input type="text" name="ExPCategory" id="ExPCategory" value="<?php echo $Res_IndExp['category'];?>" style="width:120px;" ></td>
+                            <td><textarea name="ExPNotes" id="ExPNotes" style="width: 150px; height:15px; resize: none; overflow: hidden; display: block;"><?php echo $Res_IndExp['Notes'];?></textarea></td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="2">
+                                <div style="width:130px;"><input type="checkbox" name="Staxes" id="Staxes">Taxes</div>
+                                <div style="width:130px;">
+                                    <table>
+                                        <tr>
+                                            <td>Tax 1<br><input type="text" name="taxs1" id="taxs1" style="width:50px;"></td>
+                                            <td>Amount<br><input type="text" name="taxs1Amt" id="taxs1Amt" style="width:50px;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tax 2<br><input type="text" name="taxs2" id="taxs2" style="width:50px;"></td>
+                                            <td>Amount<br><input type="text" name="taxs2Amt" id="taxs2Amt" style="width:50px;"></td>
+                                        </tr>                                        
+                                    </table>
+                                </div>
+                            </td>
+                            <td>
+                                <div style="width:110px;"><input type="checkbox" name="SRecurring" id="SRecurring">Recurring</div>
+                                <div style="width:110px;">
+                                    <table>
+                                        <tr>
+                                            <td>Frequency<br><input type="text" name="RecFreq" id="RecFreq" style="width:75px;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Until<br><input type="text" name="RecUntil" id="RecUntil" style="width:75px;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>End Date<br><input type="text" name="RecEndDate" id="RecEndDate" style="width:75px;"></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </td>
+                            <td>
+                                <div style="width:130px;"><input type="checkbox" name="SAssign" id="SAssign">Assign To Client</div>
+                                <div style="width:130px;">
+                                    <table>
+                                        <tr>
+                                            <td>Client<br><input type="text" name="AssgnCl" id="AssgnCl" style="width:75px;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Until<br><input type="text" name="AssgnProject" id="AssgnProject" style="width:75px;"></td>
+                                        </tr>                                        
+                                    </table>
+                                </div>
+                            </td>
+                            <td></td>
+                        </tr>
+
+                    </table>
+                </form>
+            </div>
+
+        <?php } ?>
+
             
 	</div>
     
    	<script src='http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script> 
+    <!-- Jquery Date Picker -->
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">    
+    <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <!-- End Jquery Date Picker -->
 	<script type="text/javascript">
 	$(document).ready(function(){
 		$('#CreateCl').hide();
@@ -106,8 +191,40 @@ small{
             $("#CreateCl").slideUp('slow');
             $('#newC').show();
         });
+
+        // Date picker
+        $(function() {
+            $( "#ExDate" ).datepicker({ dateFormat: 'dd-mm-yy' });
+            $( "#ExPDate" ).datepicker({ dateFormat: 'dd-mm-yy' });
+            $( "#RecEndDate" ).datepicker({ dateFormat: 'dd-mm-yy' });            
+        });
 	});
+
+    var specialKeys = new Array();
+        specialKeys.push(8); //Backspace
+        specialKeys.push(9); //Tab
+        specialKeys.push(46); //Delete
+        specialKeys.push(36); //Home
+        specialKeys.push(35); //End
+        specialKeys.push(37); //Left
+        specialKeys.push(39); //Right
+
+    function IsNumeric(e) {
+        var keyCode = e.keyCode == 0 ? e.charCode : e.keyCode;
+        if((keyCode >= 48 && keyCode <= 57) || (specialKeys.indexOf(e.keyCode) != -1 && e.charCode != e.keyCode))
+        {
+            var ret = ((keyCode >= 48 && keyCode <= 57) || (specialKeys.indexOf(e.keyCode) != -1 && e.charCode != e.keyCode));
+        }
+        else
+        {
+            alert('Numbers Only');
+            var ret = ((keyCode >= 48 && keyCode <= 57) || (specialKeys.indexOf(e.keyCode) != -1 && e.charCode != e.keyCode));
+        }
+        //document.getElementById("error").style.display = ret ? "none" : "inline";
+        return ret;
+    }
 	</script>
 
 </body>
+
 </html>
